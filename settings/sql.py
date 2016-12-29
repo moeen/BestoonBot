d4c4cc89b1fe7cad7379f037e864d07700854c24
Mohammad Sadegh <alirezaie.sadegh@gmail.com>
2016-12-30 02:30:05 +0330

    Mysql Database Created

--- settings/sql.py
+++ settings/sql.py
@@ -12,24 +12,15 @@ class MySQL:
             db = MySQLdb.connect(host=host, user=user, passwd=password, db=db_name)
             cursor = db.cursor()
             cursor.execute("USE " + db_name + ";" \
-                + "CREATE TABLE IF NOT EXISTS`group` (`join_status` int(10) unsigned NOT NULL,`sticker` int(10) unsigned NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;"\
-                + "LOCK TABLES `group` WRITE;"\
-                + "UNLOCK TABLES;"\
-                + "CREATE TABLE IF NOT EXISTS `users`("\
+                + "CREATE TABLE IF NOT EXISTS `users` ("\
                 + "`id` int(10) unsigned NOT NULL AUTO_INCREMENT,"\
-                + "`chatid` int(10) unsigned NOT NULL,"\
-                + "`username` varchar(45) COLLATE utf8_persian_ci DEFAULT NULL,"\
-                + "`firstname` varchar(45) COLLATE utf8_persian_ci DEFAULT NULL,"\
-                + "`lastname` varchar(45) COLLATE utf8_persian_ci DEFAULT NULL,"\
-                + "`warns` int(10) unsigned NOT NULL DEFAULT '0',"\
-                + "`kicks` int(10) unsigned NOT NULL DEFAULT '0',"\
-                + "`ban_status` int(10) unsigned NOT NULL DEFAULT '0',"\
-                + "`user_activity` int(10) unsigned NOT NULL DEFAULT '1',"\
-                + "`aliases` varchar(45) COLLATE utf8_persian_ci DEFAULT NULL,"\
-                + "`level` int(10) unsigned NOT NULL DEFAULT '1',"\
-                + "PRIMARY KEY (`id`)"
-                + ")"\
-                + "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;"\
+                + "`tg_id` int(10) unsigned NOT NULL,"\
+                + "`username` varchar(45) NOT NULL,"\
+                + "`email` varchar(45) NOT NULL,"\
+                + "`token` varchar(45) NOT NULL,"\
+                + "`password` varchar(45) NOT NULL,"\
+                + "PRIMARY KEY (`id`)"\
+                + ") ENGINE=InnoDB DEFAULT CHARSET=latin1;"\
                 + "LOCK TABLES `users` WRITE;"\
                 + "UNLOCK TABLES;")
 
