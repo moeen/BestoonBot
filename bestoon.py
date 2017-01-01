36c276d087ec2f364d8aa8d8e1a716b337d3fc4f
AlirezaieS <alirezaie.sadegh@gmail.com>
2017-01-02 01:18:52 +0330

    Exit On Wrong Settings

--- bestoon.py
+++ bestoon.py
@@ -4,32 +4,50 @@
 from telegram.ext import Updater,CommandHandler
 from settings.conf import conf
 from settings.sql import MySQL
-import os
+import os, sys
 
 class Bot_start():
     def __init__(self):
         self.token = conf.token()
+        self.flag =[]
 
         try:
             self.db_host = conf.ConfigSectionMap("database", "settings/conf.ini")["host"].strip("\"")
             self.db_username = conf.ConfigSectionMap("database", "settings/conf.ini")["user"].strip("\"")
             self.db_password = conf.ConfigSectionMap("database", "settings/conf.ini")["password"].strip("\"")
             self.db_name = conf.ConfigSectionMap("database", "settings/conf.ini")["db_name"].strip("\"")
+
         except:
             print "exception"
             self.db_host = None
             self.db_username = None
             self.db_password = None
             self.db_name = None
+
+
     def main(self):
         os.system("clear")
         mysql = MySQL()
+
         print "==============================="
         print "Starting Bestoon Bot!"
         print "==============================="
         print "Token\t\t\t:\t" + self.token
-        print "Checking principal.conf\t:\t" + conf.configCheck()
-        print "Connecting To DB\t:\t" + mysql.mysql_check(self.db_host, self.db_username, self.db_password, self.db_name)
+
+        if conf.configCheck() == "OK":
+            print "Checking principal.conf\t:\t" + conf.configCheck()
+        else:
+            print "Checking principal.conf\t:\t" + conf.configCheck()
+            sys.exit(0)
+
+        sql_check = mysql.mysql_check(self.db_host, self.db_username, self.db_password, self.db_name)
+
+        if sql_check == "OK":
+            print "Connecting To DB\t:\t" + sql_check
+        else:
+            print "Connecting To DB\t:\t" + sql_check
+            sys.exit(0)
+
         print "Bestoon Bot Started, Now Let's Get To Work!"
 
 
