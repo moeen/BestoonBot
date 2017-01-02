f830c15a530fd20b06062b06f17355e28ef76f8b
AlirezaieS <alirezaie.sadegh@gmail.com>
2017-01-02 04:03:26 +0330

    First Conversation Added :)

--- bestoon/Plugins/__init__.py
+++ bestoon/Plugins/__init__.py
@@ -3,36 +3,55 @@
 
 # PLEASE DEFINE YOUR `CommandHandler` AND `add_handler` OF EACH METHOD, RIGHT BELOW IT.
 # PLEASE ADD YOUR PLUGINS INSIDE `extPlugins` AS I DON'T MERGE ANY THIRD PARTY PLUGINS INSIDE DEFAULT PLUGINS
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from settings.conf import conf
 
 conf = conf()
 updater = Updater(str(conf.token()))
-
+SETUP , REGISTER, INTEGRATE = range(3)
 def start_method(bot, update):
     """ Start Command """
+
     startList = [["Register New Account","Integrate A Registered Account"]]
+
     chat_id = update.message.chat_id
+    replyText = update.message.text
+
     text = """Hello And Welcome To [Bestoon](http://bestoon.ir).
 This Bot Helps You Easily Access Your [Bestoon](http://bestoon.ir) Account.
 Now, How Can I Help You?
 """
     bot.sendChatAction(chat_id, "TYPING")
-    bot.sendMessage(chat_id, text, parse_mode="Markdown", reply_markup=ReplyKeyboardMarkup(startList, one_time_keyboard=True))
+    update.message.reply_text(text, parse_mode="Markdown",reply_markup=ReplyKeyboardMarkup(startList, one_time_keyboard=True))
+    return SETUP
 
-start_command = CommandHandler("start", start_method)
-updater.dispatcher.add_handler(start_command)
-
-def bye_method(bot, update):
-    """ Goodbye Command """
+def setup(bot, update):
+    """Initialize The User Account For The First Time"""
     chat_id = update.message.chat_id
-    bot.sendChatAction(chat_id, "TYPING")
-    bot.sendMessage(chat_id, "Goodbye!")
 
-goodbye_command = CommandHandler("bye", bye_method)
-updater.dispatcher.add_handler(goodbye_command)
+    if update.message.text == "Register New Account":
+        bot.sendChatAction(chat_id, "TYPING")
+        update.message.reply_text("Can\'t Register Now!")
 
+    elif update.message.text == "Integrate A Registered Account":
+        bot.sendChatAction(chat_id, "TYPING")
+        update.message.reply_text("Can\'t Integrate Now!")
+
+def bye(bot, update):
+    bot.sendMessage(update.message.chat_id, "bye!")
+
+conv_handler = ConversationHandler(
+    entry_points = [CommandHandler('start', start_method)],
+
+    states = {
+        SETUP: [MessageHandler(Filters.text, setup)]
+
+    },
+
+    fallbacks = [CommandHandler('cancel', bye)]
+)
+updater.dispatcher.add_handler(conv_handler)
 ########## Starting Bot ##########
 updater.start_polling()
 updater.idle()
