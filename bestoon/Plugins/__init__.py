8a86deb17b9ea9e93e0e370d0bdfc3630ab1ce75
AlirezaieS <alirezaie.sadegh@gmail.com>
2017-01-05 12:13:06 +0330

    Comming Soon B-)

--- bestoon/Plugins/__init__.py
+++ bestoon/Plugins/__init__.py
@@ -11,7 +11,7 @@ import os, sys
 
 conf = conf()
 updater = Updater(str(conf.token()))
-SETUP ,USERNAME,DUPLICATE = range(3)
+SETUP, USERNAME, DUPLICATE, BESTOON, DONE = range(5)
 info = []
 
 def info_a():
@@ -51,7 +51,8 @@ Now Send Me Your Token.
             update.message.reply_text(register_text,reply_markup=ReplyKeyboardRemove())
             return USERNAME
         else:
-            update.message.reply_text("It Seems This Telegram ID Has Been Integrated Before\nWould You Like To Integrate Again?")
+            List = [["Yes", "No"]]
+            update.message.reply_text("It Seems This Telegram ID Has Been Integrated Before\nWould You Like To Integrate Again?",reply_markup=ReplyKeyboardMarkup(List))
             return DUPLICATE
 
     elif update.message.text == "Register New Account":
@@ -66,29 +67,58 @@ Now Send Me Your Token.
 
 def regUser(bot, update):
     txt = update.message.text
-    if len(str(txt)) == 48:
-        info.append(update.message.text.lower())
-        update.message.reply_text("Processing Your Request,Please Wait.")
+    print txt
+    print len(txt)
+    if len(txt) == 48:
+        print info
+        info.append(txt)
+        print "text appended"
+        List = [["Finish"]]
+        update.message.reply_text("Done! Now Send Me Finish To Send You Commands", reply_markup=ReplyKeyboardMarkup(List)),
         info_a()
+        print "done!"
+        return DONE
     else:
         update.message.reply_text("Sorry, This Token Is Invalid\nPlease Retry With A Valid Token.")
 
+def remove_user(bot, update):
+    text = update.message.text
+    user = update.message.from_user
+    if text.lower() == "yes":
+        update.message.reply_text("Ok! Send Me New Token")
+        print info
+        return USERNAME
+    elif text.lower() == "no":
+        update.message.reply_text("Ok! Using Old Token!")
+        return BESTOON
 
 def cancel(bot, update):
     """ Cancel Command """
     bot.sendMessage(update.message.chat_id, "Bye!")
     return ConversationHandler.END
+def done(bot, update):
+    print "funv"
+    # bot.sendChatAction(update.message.chat_id, "TEYPING")
+    update.message.reply_text("You Can Now Use /income <value> and /expense <value> For Submiting Your Income And Expense.", reply_markup=ReplyKeyboardRemove())
+    return BESTOON
+def expense(bot, update):
+    bot.sendMessage(update.message.chat_id, "expense")
+def income(bot, update):
+    bot.sendMessage(update.message.chat_id, "Income")
 
 conv_handler = ConversationHandler(
     entry_points = [CommandHandler('start', start_method)],
 
     states = {
         SETUP: [MessageHandler(Filters.text, setup)],
-        USERNAME: [MessageHandler(Filters.text, regUser)]
-
+        USERNAME: [MessageHandler(Filters.text, regUser)],
+        DUPLICATE: [MessageHandler(Filters.text, remove_user)],
+        DONE: [MessageHandler(Filters.text, done)],
+        BESTOON: [CommandHandler("expense", expense),
+        CommandHandler("income", income)]
     },
 
-    fallbacks = [CommandHandler('cancel', cancel)]
+    fallbacks = [CommandHandler('start', start_method)]
 )
 updater.dispatcher.add_handler(conv_handler)
 ########## Starting Bot ##########
